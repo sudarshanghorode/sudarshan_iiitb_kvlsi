e380a3a02e91262ce288e4bb6951085902266174
Sudarshan Ghorode <sudarshanghorode15@gmail.com>
2025-04-14 14:36:45 +0530

    Create basic gates_tb.v

---
+++ daily_verilog_futurewiz/day1/basic gates_tb.v
@@ -0,0 +1,15 @@
+module basic_gates_tb();
+reg A,B,C;
+wire P,Q,R,S,T,U;
+  basic_gates uut(A,B,C,P,Q,R,S,T,U);
+initial begin 
+A=0;B=0;C=0; 
+A=0;B=0;C=1;
+A=0;B=1;C=0; #100;
+A=0;B=1;C=1; #100;
+A=1;B=0;C=0; #100;
+A=1;B=0;C=1; #100;
+A=1;B=1;C=0; #100;
+A=1;B=1;C=1; #10;
+end 
+endmodule
