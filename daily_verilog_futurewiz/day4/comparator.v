7501da3b9c1eb7634138deb42a52ab57d1acf237
Sudarshan Ghorode <sudarshanghorode15@gmail.com>
2025-04-17 04:37:37 +0530

    Create comparator.v

---
+++ daily_verilog_futurewiz/day4/comparator.v
@@ -0,0 +1,11 @@
+module comparator_2bit_ternary (
+  input [1:0] A,
+  input [1:0] B,
+  output A_eq_B,
+  output A_gt_B,
+  output A_lt_B
+);
+  assign A_eq_B = (A == B) ? 1'b1 : 1'b0;
+  assign A_gt_B = (A > B)  ? 1'b1 : 1'b0;
+  assign A_lt_B = (A < B)  ? 1'b1 : 1'b0;
+endmodule
