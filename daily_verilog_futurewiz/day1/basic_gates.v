a2ef6fbe1307155178c3850a5803c5dfc265924f
Sudarshan Ghorode <sudarshanghorode15@gmail.com>
2025-06-21 14:10:10 +0530

    Update basic_gates.v

--- daily_verilog_futurewiz/day1/basic_gates.v
+++ daily_verilog_futurewiz/day1/basic_gates.v
@@ -1,3 +1,4 @@
+//by gate level modeling
 module basic_gates(A,B,C,P,Q,R,S,T,U);
 input A,B,C;
 output P,Q,R,S,T,U;
@@ -8,3 +9,15 @@ nor n1(S,A,B,C);
 xor n2(T,A,B,C);
 xnor n3(U,A,B,C);
 endmodule
+
+//by data flow modeling style
+module basic_gates(A,B,C,P,Q,R,S,T,U);
+input A,B,C;
+output P,Q,R,S,T,U;
+  assign P=A&B&C;
+  assign Q=~(A&B&C);
+  assign R=A|B|C;
+  assign S=~(A|B|C);
+  assign T=A^B^C;
+  assign U=~(A^B^C);
+  endmodule
