bf02ef2e73caa901078d6732638a4fb832ef40a6
Sudarshan Ghorode <sudarshanghorode15@gmail.com>
2025-06-22 10:43:14 +0530

    Update muxusing_array.v

--- daily_verilog_futurewiz/day_3/muxusing_array.v
+++ daily_verilog_futurewiz/day_3/muxusing_array.v
@@ -1,3 +1,4 @@
+//dataflow modeling
 module mux41( 
   input [3:0]i,
   input [1:0]s,
@@ -10,3 +11,19 @@ module mux41(
     assign y4=(i[3]&s[0]&s[1]);
   assign y=y1|y2|y3|y4;
 endmodule
+
+//behavioral modeling
+module mux41(i,s,y);
+  input [3:0]i;
+  input [1:0]s;
+  output reg  y;
+
+always @(*) begin
+  case(s)
+    2'b00: y = i[0];
+    2'b01: y = i[1];
+    2'b10: y = i[2];
+    2'b11: y = i[3];
+  endcase
+end
+    
