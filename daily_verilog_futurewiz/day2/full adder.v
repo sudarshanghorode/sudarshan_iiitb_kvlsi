0af9ad1530fa91415eee4301d484dace2a1a3480
Sudarshan Ghorode <sudarshanghorode15@gmail.com>
2025-06-22 10:22:38 +0530

    Update full adder.v

--- daily_verilog_futurewiz/day2/full adder.v
+++ daily_verilog_futurewiz/day2/full adder.v
@@ -1,3 +1,4 @@
+// gatelevel modeling
 module full_adder(input a,b,cin,output s,c);
 wire w1,w2,w3;
 xor x1(w1,a,b);
@@ -7,3 +8,18 @@ and a2(w3,a,b);
 or o1(c,w2,w3);
 
 endmodule
+
+//daflow modeling
+module full_adder(input a,b,cin,output s,c);
+wire w1,w2,w3;
+  assign s=a^b^cin;
+  assign c=a&b;
+endmodule
+
+//behavioral modeling
+module full_adder(input a,b,cin,output reg s,c);
+wire w1,w2,w3;
+  always@(a,b,cin)
+  {cin,s}=a+b+cin;
+
+  end
