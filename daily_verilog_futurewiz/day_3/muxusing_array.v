088eb7fb86e60b3d83cf4eb0a43c21c247acdc80
Sudarshan Ghorode <sudarshanghorode15@gmail.com>
2025-06-22 10:54:27 +0530

    Update muxusing_array.v

--- daily_verilog_futurewiz/day_3/muxusing_array.v
+++ daily_verilog_futurewiz/day_3/muxusing_array.v
@@ -2,14 +2,15 @@
 module mux41( 
   input [3:0]i,
   input [1:0]s,
-  output y
+  output y,
+  wire w1,w2,w3,w4
 );
   
-  assign y1=(i[0]&~s[0]&~s[1]);
-  assign y2=(i[1]&~s[0]&s[1]);
-  assign y3=(i[2]&s[0]&~s[1]);
-    assign y4=(i[3]&s[0]&s[1]);
-  assign y=y1|y2|y3|y4;
+  assign w1=(i[0]&~s[0]&~s[1]);
+  assign w2=(i[1]&~s[0]&s[1]);
+  assign w3=(i[2]&s[0]&~s[1]);
+    assign w4=(i[3]&s[0]&s[1]);
+  assign y=w1|w2|w3|w4;
 endmodule
 
 //behavioral modeling
@@ -26,4 +27,13 @@ always @(*) begin
     2'b11: y = i[3];
   endcase
 end
-    
+    //by using ternary operator
+  module mux41(i,s,y);
+  input [3:0]i;
+  input [1:0]s;
+  output  y;
+
+    assign y=s[0]?(s[1]? i[3]:i[1]):(s[1]? i[2]:i[0]));
+   
+  endmodule
+  
