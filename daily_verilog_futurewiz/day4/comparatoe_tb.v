b4b0d4b7092b521d3552cce0fbec0889ec8620d9
Sudarshan Ghorode <sudarshanghorode15@gmail.com>
2025-06-22 11:45:26 +0530

    Update comparatoe_tb.v

--- daily_verilog_futurewiz/day4/comparatoe_tb.v
+++ daily_verilog_futurewiz/day4/comparatoe_tb.v
@@ -1,10 +1,10 @@
-module camparator_2bit_ternary_tb;
+module camparator2bittb;
   reg [1:0]A;
   reg [1:0]B;
   wire A_eq_B;
   wire A_gt_B;
   wire A_lt_B;
-  comparator_2bit_ternary dut(A,B,A_eq_B,A_gt_B,A_lt_B);
+  comparator2bit dut(A,B,A_eq_B,A_gt_B,A_lt_B);
   initial begin
    A=2'b00;B=2'b00;
  #5  A=2'b10;B=2'b00;
